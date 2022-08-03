adef4ace4b6002b334570382601ffe1bb5371b46
Keshavroy12345 <Keshu1123@gmail.com>
2022-08-03 11:00:31 +0530

    keshav

--- Newproject/src/test/java/NewClass/CLassNmeTest.java
+++ Newproject/src/test/java/NewClass/CLassNmeTest.java
@@ -13,6 +13,10 @@ public class CLassNmeTest {
 	public void smile() {
 		System.out.println("Test2");
 		System.out.println("Test3");
+	}
+	@Test
+	public void session() {
+		System.out.println("Test4");
 		
 			
 		}
